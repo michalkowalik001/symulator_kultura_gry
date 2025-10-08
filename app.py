b475c80395e88c4d9aa06376dbdf2d98a87d70fe
michalkowalik001 <michalkowalik001@gmail.com>
2025-10-08 22:09:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ src="https://sy0kh.mjt.lu/wgt/sy0kh/0phq/form?c=f6f3e641" width="100%" style="he
 <script type="text/javascript" src="https://app.mailjet.com/pas-nc-embedded-v1.js"></script>
 """
 
-content_1 = f"""
+content_1 = """
 <p><b>Chcemy stworzyć kurs, w którym pokażemy jak zostać analitycznym magikiem i być w stanie przewidywać i symulować wyniki meczów i rozgrywek ligowych. To wcale nie takie trudne.</b></p>
 
 <p>Niezależnie czy analizujesz dane na co dzień, czy jesteś zupełnie zielony i nie masz pojęcia jak się do tego zabrać. Przeprowadzimy Cię przez cały proces od A do Z.</p>
@@ -75,13 +75,18 @@ content_1 = f"""
 <p><b>Lub zapisz się na newsletter, w którym wyślemy Ci postępy w tworzeniu kursu!</b></p>
 """
 
-st.markdown(card_style.format(content=content_1), unsafe_allow_html=True)
-components.html(html_code, height=400, scrolling=False)
+# wstawiamy zawartość bezpośrednio, używając f-string z podwójnymi klamrami w card_style
+st.markdown(f"""
+<div style="
+    background-color: #f9f9f9; 
+    border-radius: 15px; 
+    padding: 20px; 
+    margin-bottom: 20px;
+    box-shadow: 0 4px 10px rgba(0,0,0,0.1);
+">
+    {content_1}
+</div>
+""", unsafe_allow_html=True)
 
-# Druga karta - buy_coffee
-content_2 = """
-<p>☕ Wspomóż naszą pracę i rozwój kursu! </p>
-"""
-st.markdown(card_style.format(content=content_2), unsafe_allow_html=True)
-buy_cofee()
+components.html(html_code, height=400, scrolling=False)
 
