016945f0ef7a92f46fe4c03d12143ca6dbf8c36a
michalkowalik001 <michalkowalik001@gmail.com>
2025-10-08 18:46:41 +0200

    Update sidebar.py

--- utils/sidebar.py
+++ utils/sidebar.py
@@ -21,8 +21,8 @@ html_code = """
     """
 
 def show_sidebar():
-    logo_path = os.path.join(BASE_DIR, "images", "logo_czarne.png")
-    st.sidebar.image(logo_path, width=600)
+    #logo_path = os.path.join(BASE_DIR, "images", "logo_czarne.png")
+    #st.sidebar.image(logo_path, width=600)
 
     st.sidebar.markdown("### Nawigacja")
     st.sidebar.markdown("- Strona główna")
