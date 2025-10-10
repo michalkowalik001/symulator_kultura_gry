81aa8e45eecf1e177a87cdf3643a1ddac8a1bd0f
michalkowalik001 <michalkowalik001@gmail.com>
2025-10-10 13:02:24 +0200

    Update Metodologia.py

--- pages/Metodologia.py
+++ pages/Metodologia.py
@@ -39,7 +39,5 @@ st.write("Tabela 'Najbliższe mecze' została uszeregowana malejąco według kol
 
 st.write("W tabeli 'Szanse na mistrzostwo' i na wykresie 'Szanse na poszczególne pozycje', obliczonych symulacyjnie zgodnie z Kapich (2025) uwzględniono przewagę własnego boiska.")
 
-st.write("Założenia symulacji: 
-1. Niezmienność ELO w przyszłości
-2. Przewaga własnego boiska = 63 ELO")
+st.write("Założenia symulacji: 1. Niezmienność ELO w przyszłości; 2. Przewaga własnego boiska = 63 ELO")
 
